5229a596ed4113b07b9ea02c4a3de2f5c2c5452f
Luka Giorgadze <luka.giorgadze@gmail.com>
2025-10-29 21:51:12 +0400

    multi payment provider

--- packages/modules/b2c-core/src/modules/payout/index.ts
+++ packages/modules/b2c-core/src/modules/payout/index.ts
@@ -1,21 +1,12 @@
 import { Module } from "@medusajs/framework/utils";
 
 import PayoutModuleService from "./service";
-import { StripePayoutProvider, AdyenPayoutProvider } from "./services";
+import payoutModuleLoader from "./loaders";
 
 export const PAYOUT_MODULE = "payout";
 export { PayoutModuleService };
 
 export default Module(PAYOUT_MODULE, {
   service: PayoutModuleService,
-  providers: [
-    {
-      register: "stripePayoutProvider",
-      useClass: StripePayoutProvider,
-    },
-    {
-      register: "adyenPayoutProvider",
-      useClass: AdyenPayoutProvider,
-    },
-  ],
+  loaders: [payoutModuleLoader],
 });
