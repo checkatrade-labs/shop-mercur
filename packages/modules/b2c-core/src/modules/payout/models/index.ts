3a5d432a31cfe73e5d822900c5222ff1e904de1a
Luka Giorgadze <luka.giorgadze@gmail.com>
2025-11-19 15:23:19 -0500

    before stash

--- packages/modules/b2c-core/src/modules/payout/models/index.ts
+++ packages/modules/b2c-core/src/modules/payout/models/index.ts
@@ -2,8 +2,4 @@ export * from './payout-account'
 export * from './payout'
 export * from './onboarding'
 export * from './payout-reversal'
-<<<<<<< Updated upstream
 export * from './payment-webhook'
-=======
-export * from './payment-webhook'
->>>>>>> Stashed changes
